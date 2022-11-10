5e1902bf067ef7a3301278ad2a01f24b90c8f128
Yasmin ElShazly <yasminashazly@gmail.com>
2022-11-10 15:25:02 +0200

    delete(frontend): unnecessary files

--- angular-14-client/src/app/app-routing.module.ts
+++ angular-14-client/src/app/app-routing.module.ts
@@ -1,29 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TutorialsListComponent } from './components/tutorial/tutorials-list/tutorials-list.component';
-import { TutorialDetailsComponent } from './components/tutorial/tutorial-details/tutorial-details.component';
 import { IndexComponent } from './views/index/index.component';
 import { RegisterComponent } from "./views/auth/register/register.component";
-import {ConfirmComponent} from "./views/auth/confirmation/confirm.component";
-import {StationListComponent} from "./components/station/station-list/station-list.component";
-import {StationDetailsComponent} from "./components/station/station-details/station-details.component";
-import {AddTripComponent} from "./components/trip/add-trip/add-trip.component";
-import {AddStationComponent} from "./components/station/add-station/add-station.component";
-import {AddTutorialComponent} from "./components/tutorial/add-tutorial/add-tutorial.component";
-import {TripListComponent} from "./components/trip/trip-list/trip-list.component";
-import {TripDetailsComponent} from "./components/trip/trip-details/trip-details.component";
+import { ConfirmComponent } from "./views/auth/confirmation/confirm.component";
+import { AddTripComponent } from "./components/trip/add-trip/add-trip.component";
+import { TripListComponent } from "./components/trip/trip-list/trip-list.component";
+import { TripDetailsComponent } from "./components/trip/trip-details/trip-details.component";
+import { AddStationComponent } from './components/station/add-station/add-station.component';
+import { StationListComponent } from './components/station/station-list/station-list.component';
+import { StationDetailsComponent } from './components/station/station-details/station-details.component';
 
 const routes: Routes = [
   { path: '', component: IndexComponent},
+  
   { path: 'register', component: RegisterComponent},
   { path: 'success', component: ConfirmComponent},
-  { path: 'tutorials', component: TutorialsListComponent },
   { path: 'stations', component: StationListComponent },
   { path: 'trips', component: TripListComponent },
   { path: 'stations/get/:id', component: StationDetailsComponent },
   { path: 'trips/get/:id', component: TripDetailsComponent },
-  { path: 'tutorials/:id', component: TutorialDetailsComponent },
-  { path: 'add', component: AddTutorialComponent },
   { path: 'add/trip', component: AddTripComponent },
   { path: 'add/station', component: AddStationComponent }
 ];
--- angular-14-client/src/app/app.module.ts
+++ angular-14-client/src/app/app.module.ts
@@ -5,26 +5,20 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddTutorialComponent } from './components/tutorial/add-tutorial/add-tutorial.component';
-import { TutorialDetailsComponent } from './components/tutorial/tutorial-details/tutorial-details.component';
-import { TutorialsListComponent } from './components/tutorial/tutorials-list/tutorials-list.component';
 import { IndexComponent } from './views/index/index.component';
 import {RegisterComponent} from "./views/auth/register/register.component";
 import {ConfirmComponent} from "./views/auth/confirmation/confirm.component";
-import {StationListComponent} from './components/station/station-list/station-list.component';
-import {StationDetailsComponent} from "./components/station/station-details/station-details.component";
 import {AddTripComponent} from "./components/trip/add-trip/add-trip.component";
-import {AddStationComponent} from "./components/station/add-station/add-station.component";
 import {TripDetailsComponent} from "./components/trip/trip-details/trip-details.component";
 import {TripListComponent} from "./components/trip/trip-list/trip-list.component";
+import { StationListComponent } from './components/station/station-list/station-list.component';
+import { AddStationComponent } from './components/station/add-station/add-station.component';
+import { StationDetailsComponent } from './components/station/station-details/station-details.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddTutorialComponent,
-    TutorialDetailsComponent,
-    TutorialsListComponent,
     IndexComponent,
     RegisterComponent,
     ConfirmComponent,
